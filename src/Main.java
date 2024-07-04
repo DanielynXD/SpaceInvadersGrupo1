7033694c08026b9b36d424a5e52c6859e69f13ad
Chauca-Aaron48 <aaron.chauca@epn.edu.ec>
2024-07-03 22:02:59 -0500

    Fealings

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
         System.out.println("prueba deaaah" + 2*2*2*2*2*2*2*2*2*2*2);
         System.out.println("Nicky estuvo aqui");
 
+        System.out.println("MI SEGUNDA PRUEBA JAJAJAJA");
+        System.out.println("Me siento enamorado");
 
 
     }
