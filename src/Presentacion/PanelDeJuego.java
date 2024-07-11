582ddb5a1866e9e2d99f2b4c8a81a6f5d1f0695b
DanielynXD <diego.lloay@epn.edu.ec>
2024-07-11 18:51:24 -0500

    Realizar codigo proyectil jugador Diego

--- src/Presentacion/PanelDeJuego.java
+++ src/Presentacion/PanelDeJuego.java
@@ -2,6 +2,7 @@ package Presentacion;
 
 import Logica.NaveEnemigoUno;
 import Logica.NaveJugador;
+import Logica.Proyectil;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -10,6 +11,7 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.util.Objects;
 import javax.swing.*;
+import java.util.List;
 
 public class PanelDeJuego extends JPanel implements ActionListener {
 
@@ -26,7 +28,7 @@ public class PanelDeJuego extends JPanel implements ActionListener {
     private void iniciarPanel() {
         setFocusable(true);
         setSize(ANCHO, ALTO);
-        nave = new NaveJugador();
+        nave = new NaveJugador(ANCHO, ALTO);
         naveEnemigoUno = new NaveEnemigoUno();
         fondo = new ImageIcon(Objects.requireNonNull(PanelDeJuego.class.getResource("/ImagenesJuego/Fondos/FondoEscena.png"))).getImage();
         addKeyListener(new TAdapter());
@@ -41,7 +43,17 @@ public class PanelDeJuego extends JPanel implements ActionListener {
         dibujarFondo(g);
         dibujarNave(g);
         dibujarEnemigoUno(g);
+        dibujarProyectiles(g);
         Toolkit.getDefaultToolkit().sync();
+    }
+
+    private void dibujarProyectiles(Graphics g) {
+        List<Proyectil> proyectiles = nave.obtenerProyectiles();
+        for (Proyectil p : proyectiles) {
+            if(p.esVisible()){
+                g.drawImage(p.obtenerImagen(), p.obtenerX(), p.obtenerY(), this);
+            }
+        }
     }
 
     private void dibujarFondo(Graphics g) {
@@ -59,7 +71,20 @@ public class PanelDeJuego extends JPanel implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         actualizarNave();
+        actualizarProyectiles();
         repaint();
+    }
+
+    private void actualizarProyectiles() {
+        List<Proyectil> proyectiles = nave.obtenerProyectiles();
+        for (int i = 0; i < proyectiles.size(); i++) {
+            Proyectil proyectil = proyectiles.get(i);
+            if (proyectil.esVisible()) {
+                proyectil.mover();
+            } else {
+                proyectiles.remove(i);
+            }
+        }
     }
 
     private void actualizarNave() {
